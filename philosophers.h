46ebb97b06ae9a7e669e34a2bef8e45a20cffd2f
Fredi-B <fredibechtoldt@outlook.com>
2022-03-15 12:33:29 +0100

    added header file

---
+++ philosophers.h
@@ -0,0 +1,36 @@
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+/* ----------------------------- Includes ---------------------------------- */
+# include <stdio.h>
+# include <sys/time.h>
+/* ----------------------------- Defines ----------------------------------- */
+
+/* # define TRUE 1
+# define FALSE 0
+# define OK 0
+# define ERROR -2 */
+
+/* ---------------------- Defines for debugging ---------------------------- */
+
+/* # define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+# define dwrite(expr) write(2, "\n" #expr "\n", strlen(#expr) + 2)
+ */
+
+/* ----------------------------- Structures -------------------------------- */
+
+/* typedef struct s_in_out
+{
+	char			*name;
+	char			*here_doc;
+	int				mode;
+	struct s_in_out	*next;
+
+}				t_in_out;
+ */
+/*  ------------------------ Function prototypes --------------------------- */
+
+
+#endif
