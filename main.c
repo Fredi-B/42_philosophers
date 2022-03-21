4a7245efa7d7c0dfcf4f9630bb9bc74b4d5a9a4d
Fredi-B <fredibechtoldt@outlook.com>
2022-03-21 18:13:12 +0100

    improved error handling and added init_threads()

--- main.c
+++ main.c
@@ -5,8 +5,10 @@ int	main(int argc, char **argv)
 	t_data	data;
 
 	if (parsing(&data, argc, argv) == ERROR)
-		return (1);
+		exit(1);
 	if (init_mutexes(&data) == ERROR)
-		return (2);
+		err_exit(&data, 2, "Error in init_mutexes\n", 22);
+	if (init_threads(&data) == ERROR)
+		err_exit(&data, 3, "Error in init_threads\n", 22);
 	return (0);
 }
