8fc5786b11cdaf6f18fcd41f2feec5b2b5178298
Fredi-B <fredibechtoldt@outlook.com>
2022-03-27 23:27:06 +0200

    cosmetics and debug prints

--- doctor.c
+++ doctor.c
@@ -5,7 +5,7 @@ void	*doctor(void *arg)
 	t_data			**philosophers;
 	struct timeval	tv;
 	unsigned long	current_time;
-	long	time_passed;
+	long			time_passed;
 	int				i;
 
 	philosophers = (t_data **)arg;
@@ -16,10 +16,13 @@ void	*doctor(void *arg)
 		gettimeofday(&tv, NULL);
 		current_time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 		time_passed = (current_time - *philosophers[i]->time_last_eaten);
+
 		if (time_passed > philosophers[i]->time_to_die)
 		{
 			protected_print(philosophers[i], DIED);
 			*philosophers[i]->died = TRUE;
+			printf("time passed %li\n", time_passed);
+			printf("%i time last eaten %li\n", philosophers[i]->philosopher, *philosophers[i]->time_last_eaten - *philosophers[1]->start_time);
 			break ;
 		}
 		i++;
