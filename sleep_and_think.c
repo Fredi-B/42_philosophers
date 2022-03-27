63265e31092b90925f5c84105ade6662743e65e9
Fredi-B <fredibechtoldt@outlook.com>
2022-03-27 23:28:32 +0200

    added usleep if there are lots of philosophers

--- sleep_and_think.c
+++ sleep_and_think.c
@@ -5,5 +5,7 @@ void	sleep_and_think(t_data *philosopher)
 	protected_print(philosopher, SLEEP);
 	usleep(philosopher->time_to_sleep * 1000);
 	protected_print(philosopher, THINK);
-	usleep(50);
+	if (philosopher->total_number_of_p > 30)
+		usleep(650);
+	usleep(200);
 }
