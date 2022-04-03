7e88b3a20d1d5a70b156f54bce743a81a164c1a9
Fredi-B <fredibechtoldt@outlook.com>
2022-04-03 20:07:55 +0200

    fixed 1 philosopher segfault by changing philosophers[1] to [0]

--- doctor.c
+++ doctor.c
@@ -1,6 +1,7 @@
 #include "philosophers.h"
 
 static void	update_runtime(t_data *philosopher);
+static void	doctor_sleeps(t_data *philosopher);
 
 void	*doctor(void *arg)
 {
@@ -10,23 +11,24 @@ void	*doctor(void *arg)
 	int				i;
 
 	philosophers = (t_data **)arg;
+	update_runtime(philosophers[0]);
 	doctor_sleeps(philosophers[0]);
 	i = 0;
 	while (1)
 	{
-		*philosophers[1]->runtime = get_time() - *philosophers[1]->start_time;
 		current_time = get_time();
+		*philosophers[0]->runtime = current_time - *philosophers[0]->start_time;
 		time_passed = (current_time - *philosophers[i]->time_last_eaten);
-		if (time_passed > philosophers[i]->time_to_die)
+		if (time_passed >= philosophers[i]->time_to_die)
 		{
 			protected_print(philosophers[i], DIED);
 			*philosophers[i]->died = TRUE;
 			printf("time passed %li\n", time_passed);
-			printf("%i time last eaten %li\n", philosophers[i]->philosopher, *philosophers[i]->time_last_eaten - *philosophers[1]->start_time);
+			printf("%i time last eaten %li\n", philosophers[i]->philosopher, *philosophers[i]->time_last_eaten - *philosophers[0]->start_time);
 			break ;
 		}
 		i++;
-		if (i == philosophers[1]->total_number_of_p)
+		if (i == philosophers[0]->total_number_of_p)
 			i = 0;
 	}
 	return (OK);
