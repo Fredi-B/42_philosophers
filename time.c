279d743da3a90a69519a00015561aae35e31e65f
Fredi-B <fredibechtoldt@outlook.com>
2022-04-04 14:45:48 +0200

    changed to long long

--- time.c
+++ time.c
@@ -1,9 +1,9 @@
 #include "philosophers.h"
 
-unsigned long	get_time(void)
+long long	get_time(void)
 {
 	struct timeval	tv;
-	unsigned long	current_time;
+	long long	current_time;
 
 	gettimeofday(&tv, NULL);
 	current_time = ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
