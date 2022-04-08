a816383cbc5657d630d1b2713a462f00b28f6fcd
Fredi-B <fredibechtoldt@outlook.com>
2022-04-08 10:58:46 +0200

    wrote free fct for the frees

--- init_mutexes.c
+++ init_mutexes.c
@@ -38,37 +38,5 @@ static int	init_print_mutex(t_data *data)
 		return (ERROR);
 	}
 	pthread_mutex_init(data->print_mutex, NULL);
-	// to do: free data->cnt_number_of_p --> check
-	data->cnt_number_of_p = malloc(sizeof(int));
-	if (!data->cnt_number_of_p)
-	{
-		write(2, "Error: malloc cnt_number_of_p\n", 30);
-		return (ERROR);
-	}
-	*data->cnt_number_of_p = 0;
-	// to do: free data->start_time --> check
-	data->start_time = malloc(sizeof(long long));
-	if (!data->start_time)
-	{
-		write(2, "Error: malloc start_time\n", 25);
-		return (ERROR);
-	}
-	*data->start_time = 0;
-	// to do: free data->enough_meals --> check
-	data->enough_meals = malloc(sizeof(int));
-	if (!data->enough_meals)
-	{
-		write(2, "Error: malloc enough_meals\n", 27);
-		return (ERROR);
-	}
-	*data->enough_meals = 0;
-	// to do: free data->died --> check
-	data->died = malloc(sizeof(int));
-	if (!data->died)
-	{
-		write(2, "Error: malloc died\n", 19);
-		return (ERROR);
-	}
-	*data->died = FALSE;
 	return (OK);
 }
