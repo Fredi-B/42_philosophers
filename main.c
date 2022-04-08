cea03208492efca9505026ca2798c8e1496f9123
Fredi-B <fredibechtoldt@outlook.com>
2022-04-08 11:04:12 +0200

    added init_pointer()

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int	main(int argc, char **argv)
 		exit(1);
 	if (init_mutexes(&data) == ERROR)
 		err_exit(&data, 2, "Error in init_mutexes\n", 22);
+	if (init_pointer(&data) == ERROR)
+		err_exit(&data, 3, "Error in init_pointer\n", 22);
 	if (init_threads(&data) == ERROR)
 		err_exit(&data, 3, "Error in init_threads\n", 22);
 	free_data(&data);
