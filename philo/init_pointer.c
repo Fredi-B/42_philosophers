02666850e13e64e6e92f631f513536f51e249d0d
Fredi-B <fredibechtoldt@outlook.com>
2022-04-12 20:17:06 +0200

    made protection actually protect

--- philo/init_pointer.c
+++ philo/init_pointer.c
@@ -6,7 +6,7 @@
 /*   By: fbechtol <fbechtol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:11:14 by fbechtol          #+#    #+#             */
-/*   Updated: 2022/04/11 16:11:15 by fbechtol         ###   ########.fr       */
+/*   Updated: 2022/04/12 17:33:44 by fbechtol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,8 @@
 int	init_pointer(t_data *data)
 {
 	data->cnt_number_of_p = malloc(sizeof(int));
-	*data->cnt_number_of_p = 0;
 	data->start_time = malloc(sizeof(long long));
-	*data->start_time = 0;
 	data->enough_meals = malloc(sizeof(int));
-	*data->enough_meals = 0;
 	data->died = malloc(sizeof(int));
 	if (!data->died || !data->enough_meals || !data->start_time \
 			|| !data->cnt_number_of_p)
@@ -27,6 +24,9 @@ int	init_pointer(t_data *data)
 		write(2, "Error: malloc pointer\n", 22);
 		return (ERROR);
 	}
+	*data->cnt_number_of_p = 0;
+	*data->start_time = 0;
+	*data->enough_meals = 0;
 	*data->died = FALSE;
 	return (OK);
 }
