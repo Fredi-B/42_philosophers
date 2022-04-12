5ff0e011d5a1352a252e4db9d45f90705686c901
Fredi-B <fredibechtoldt@outlook.com>
2022-04-12 13:15:48 +0200

    fixed dying delay with restructuring sleep_and_think()

--- sleep_and_think.c
+++ sleep_and_think.c
@@ -6,7 +6,7 @@
 /*   By: fbechtol <fbechtol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:11:49 by fbechtol          #+#    #+#             */
-/*   Updated: 2022/04/11 16:11:50 by fbechtol         ###   ########.fr       */
+/*   Updated: 2022/04/12 13:10:49 by fbechtol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,11 @@ void	sleep_and_think(t_data *philosopher)
 {
 	long long	current_time;
 	long long	time_passed;
-	int			someone_died;
 
 	protected_print(philosopher, "is sleeping\n", SLEEP);
 	current_time = get_time();
 	time_passed = (current_time - *philosopher->time_last_eaten);
-	if (time_passed + philosopher->time_to_sleep >= philosopher->time_to_die)
-	{
-		someone_died = FALSE;
-		while (someone_died == FALSE)
-		{
-			pthread_mutex_lock(philosopher->enough_mutex);
-			if (*philosopher->died == TRUE)
-				someone_died = TRUE;
-			pthread_mutex_unlock(philosopher->enough_mutex);
-		}
-	}
-	else
-		ft_sleep(philosopher, current_time, philosopher->time_to_sleep);
+	ft_sleep(philosopher, current_time, philosopher->time_to_sleep);
 	protected_print(philosopher, "is thinking\n", THINK);
 	sync_philosophers(philosopher);
 }
@@ -44,15 +31,23 @@ void	sleep_and_think(t_data *philosopher)
 void	ft_sleep(t_data *philosopher, long long current_time, long long chill)
 {
 	long long	start_time;
+	int			someone_died;
 
+	someone_died = FALSE;
 	start_time = current_time;
 	while (current_time < start_time + chill)
 	{
-		current_time = get_time();
+		pthread_mutex_lock(philosopher->enough_mutex);
+		if (*philosopher->died == TRUE)
+			someone_died = TRUE;
+		pthread_mutex_unlock(philosopher->enough_mutex);
+		if (someone_died == TRUE)
+			break ;
 		if (philosopher->total_number_of_p > 50)
 			usleep(philosopher->total_number_of_p);
 		else
 			usleep(50);
+		current_time = get_time();
 	}
 }
 
