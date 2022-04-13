1022fe2f9ecae49f740978ca82da71f513c58ed6
Fredi-B <fredibechtoldt@outlook.com>
2022-04-13 21:34:51 +0200

    first steps. work in progress

--- philo_bonus/routine_bonus.c
+++ philo_bonus/routine_bonus.c
@@ -1,7 +1,22 @@
 #include "philosophers_bonus.h"
 
+static void	eat_sleep_repeat(t_data *data);
+
 void	routine(t_data *data)
 {
 	sem_wait(data->wait_for_children);
 	sem_post(data->wait_for_children);
+	data->start_time = ft_get_time();
+	data->time_last_eaten = data->start_time;
+	if (data->philosopher % 2 && data->total_number_of_p != 1)
+		usleep(data->time_to_eat * 100);
+	eat_sleep_repeat(data);
+}
+
+static void	eat_sleep_repeat(t_data *data)
+{
+	while (1)
+	{
+		eat(data);
+	}
 }
