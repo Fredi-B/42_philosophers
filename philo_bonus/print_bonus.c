f0df8fb88ad5614455d57687b836787c81761ce2
Fredi-B <fredibechtoldt@outlook.com>
2022-04-14 16:28:09 +0200

    actually protected protected_print()

--- philo_bonus/print_bonus.c
+++ philo_bonus/print_bonus.c
@@ -7,6 +7,8 @@ long long	protected_print(t_data *data, char *action)
 
 	current_time = ft_get_time();
 	runtime = current_time - data->start_time;
+	sem_wait(data->print_sem);
 	printf("%lli %i %s", runtime, data->philosopher, action);
+	sem_post(data->print_sem);
 	return (current_time);
 }
