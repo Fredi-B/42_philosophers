f7e81b746b866c24fbb7dada24c820b72366d4a9
Fredi-B <fredibechtoldt@outlook.com>
2022-04-14 14:31:56 +0200

    completed first draft of eat()

--- philo_bonus/eat_bonus.c
+++ philo_bonus/eat_bonus.c
@@ -1,12 +1,15 @@
 #include "philosophers_bonus.h"
 
 static void	take_forks(t_data *data);
+static void	put_forks(t_data *data);
 
 void	eat(t_data *data)
 {
 	take_forks(data);
-	protected_print(data, "is eating\n");
-
+	data->time_last_eaten = protected_print(data, "is eating\n");
+	ft_sleep(data->time_last_eaten, data->time_to_eat);
+	data->times_eaten++;
+	put_forks(data);
 }
 
 static void	take_forks(t_data *data)
@@ -15,4 +18,10 @@ static void	take_forks(t_data *data)
 	protected_print(data, "has taken a fork\n");
 	sem_wait(data->cutlery_sem);
 	protected_print(data, "has taken a fork\n");
+}
+
+static void	put_forks(t_data *data)
+{
+	sem_post(data->cutlery_sem);
+	sem_post(data->cutlery_sem);
 }
