e3daf6bfa22679f507ac34720a802612362ae582
Fredi-B <fredibechtoldt@outlook.com>
2022-04-14 14:35:06 +0200

    wrote one_philosopher(). seems to work

--- philo_bonus/routine_bonus.c
+++ philo_bonus/routine_bonus.c
@@ -1,5 +1,6 @@
 #include "philosophers_bonus.h"
 
+static int	one_philosopher(t_data *data);
 static void	eat_sleep_repeat(t_data *data);
 
 void	routine(t_data *data)
@@ -8,6 +9,8 @@ void	routine(t_data *data)
 	sem_post(data->wait_for_children);
 	data->start_time = ft_get_time();
 	data->time_last_eaten = data->start_time;
+	if (one_philosopher(data) == TRUE)
+		exit(0);
 	if (data->philosopher % 2 && data->total_number_of_p != 1)
 		usleep(data->time_to_eat * 100);
 	eat_sleep_repeat(data);
@@ -19,4 +22,17 @@ static void	eat_sleep_repeat(t_data *data)
 	{
 		eat(data);
 	}
+static int	one_philosopher(t_data *data)
+{
+	long long	current_time;
+
+	if (data->total_number_of_p == 1)
+	{
+		sem_wait(data->cutlery_sem);
+		current_time = protected_print(data, "has taken a fork\n");
+		ft_sleep(current_time, data->time_to_die);
+		protected_print(data, "died\n");
+		return (TRUE);
+	}
+	return (FALSE);
 }
