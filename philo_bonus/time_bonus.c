32285962a71c47b14c4880be3f140193309a7396
Fredi-B <fredibechtoldt@outlook.com>
2022-04-14 14:40:31 +0200

    wrote ft_sleep(). usleep seems to be important

--- philo_bonus/time_bonus.c
+++ philo_bonus/time_bonus.c
@@ -7,3 +7,15 @@ long long	ft_get_time(void)
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
+
+void	ft_sleep(long long current_time, long long chill)
+{
+	long long	start_time;
+
+	start_time = current_time;
+	while (current_time < start_time + chill)
+	{
+		usleep(50);
+		current_time = ft_get_time();
+	}
+}
